e806bb252e2a8dc26ec9dc8dee0c6330eff67d3f
AfanasyevAA6 <alexafan27041998@gmail.com>
2021-02-15 22:53:17 +0300

    2 missin complete

--- shedule_section_reader.py
+++ shedule_section_reader.py
@@ -1,13 +1,35 @@
-import pandas as pd
+from lib import schedule_lib as sl
 
-shedule_file = input('Введите путь shedule файла: ') # ввод пути файла shedule секции
+schedule_name = input('Введите имя shedule файла: ')  # ввод пути файла shedule секции
+schedule_file = 'input' + '\\' + schedule_name
+print(schedule_file)
 count = 0
 
-with open(shedule_file, "r", encoding = "UTF-8") as file:
-    for lines in file: count += 1 # определение кол-ва строк
-          
-with open(shedule_file, "r", encoding="UTF-8") as file: 
-    line = file.readlines() # считывание строк
+with open(schedule_file, "r", encoding="UTF-8") as file:
+    line = file.readlines()  # считывание строк
+count = len(line)
 
-for i in range(count):
-    print(line[i])
+clean_schedule_name = 'handled_schedule.inc'
+# создает новый путь очищенного файла
+clean_schedule_file_path = "output" + '\\' + clean_schedule_name
+
+if __name__ == "__main__":
+    # список очищенных строк
+    line_clean = []
+    line_clean = sl.clean_schedule(line, clean_schedule_file_path, count)
+    count_clean = len(line_clean)
+
+    # список дат
+    data_list = []
+    data_list = sl.parse_keyword_DATE_line(line_clean, count_clean)
+    print("DATES", "\n", data_list)
+
+    # список COMPDAT
+    compdat_list = []
+    compdat_list = sl.parse_keyword_COMPDAT_line(line_clean, count_clean)
+    print("COMPDAT", "\n", compdat_list)
+
+    # список COMPDATL
+    compdatl_list = []
+    compdatl_list = sl.parse_keyword_COMPDATL_line(line_clean, count_clean)
+    print("COMPDATL", "\n", compdatl_list)
